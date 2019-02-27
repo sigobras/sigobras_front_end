1f40bf1482f5b1ae5bc71f35db2b387005e04619
ANGerman95 <german.apaza.n@gmail.com>
2019-02-27 12:23:38 -0500

    url

--- src/js/components/Utils/ServerUrlConfig.js
+++ src/js/components/Utils/ServerUrlConfig.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
 // const UrlServer = "https://sigobrasbeta.herokuapp.com";
-const UrlServer = "http://190.117.94.80:9000";
+const UrlServer = "http://localhost:9000";
+// const UrlServer = "http://190.117.94.80:9000";
 
 //anton -------- en la red  ( oficina )
 // const UrlServer = "http://192.168.0.4:9000"; 
