ad21d002f05f8dd090ffd2a1704d1b8607f53737
ANGerman95 <german.apaza.n@gmail.com>
2019-04-01 13:12:11 -0500

    cronograma

--- src/js/components/Inicio/componetsInicio/CronogramaAvance.js
+++ src/js/components/Inicio/componetsInicio/CronogramaAvance.js
@@ -16,8 +16,22 @@ class CronogramaAvance extends Component{
           type: 'line',
           backgroundColor: '#2e3742',
         },
+
+        legend: {
+          itemStyle: {
+              color: '#E0E0E3'
+          },
+          itemHoverStyle: {
+              color: '#ffffff'
+          },
+          itemHiddenStyle: {
+              color: '#606063'
+          }
+        },
+
         title: {
-          text: 'CURVA "S"'
+          text: 'CURVA "S"',
+          color: '#ffffff'
         },
         // subtitle: {
         //   text: 'ing joven' QUI IRA LA OBRA
@@ -35,7 +49,7 @@ class CronogramaAvance extends Component{
             dataLabels: {
               enabled: true
             },
-            enableMouseTracking: false
+            enableMouseTracking: true
           }
         },
         series: [{
@@ -66,6 +80,66 @@ class CronogramaAvance extends Component{
                 // constructorType={'stockChart'}
                 options={options}
             />
+            <table className="table table-sm ">
+              <thead>
+                <tr>
+                  <th className="border text-center" colSpan="5">MONTOS VALORIZADOS PROGRAMADOS</th>
+                  <th className="border text-center" colSpan="6">MONTOS VALORIZADOS EJECUTADOS</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border " colSpan="2">Periodo</td>
+                  <td className="border " colSpan="3">Programado</td>
+                  <td className="border " colSpan="3">Fisico Ejecutado</td>
+                  <td className="border " colSpan="3">Financiero Ejecutado</td>
+                </tr>
+
+                <tr>
+                  <td className="border ">Nº de informe</td>
+                  <td className="border ">Mes del informe</td>
+                  <td className="border ">Monto S/.</td>
+                  <td className="border ">% Ejecucion programada</td>
+                  <td className="border ">% Acumulado</td>
+                  <td className="border ">Monto S/.</td>
+                  <td className="border ">% Ejecucion programada</td>
+                  <td className="border ">% Acumulado</td>
+                  <td className="border ">Monto S/.</td>
+                  <td className="border ">% Ejecucion programada</td>
+                  <td className="border ">% Acumulado</td>
+                </tr>
+
+                <tr>
+                  <td className="border ">1</td>
+                  <td className="border ">4</td>
+                  <td className="border "></td>
+                  <td className="border "></td>
+                  <td className="border ">0</td>
+                  <td className="border "></td>
+                  <td className="border "></td>
+                  <td className="border ">0</td>
+                  <td className="border "></td>
+                  <td className="border "></td>
+                  <td className="border ">0</td>
+                </tr>
+
+
+                <tr>
+                  <td className="border" colSpan="2">Total a la Fecha</td>
+                  <td className="border ">1235</td>
+                  <td className="border ">21313</td>
+                  <td className="border  border-bottom-0"></td>
+                  <td className="border ">53453453</td>
+                  <td className="border ">53543</td>
+                  <td className="border  border-bottom-0"></td>
+                  <td className="border ">46456</td>
+                  <td className="border  ">45654654</td>
+
+                  <td className="border  border-bottom-0 border-right-0"></td>
+                </tr>
+              </tbody>
+            </table>
+
           </div>
       </div>
     )
