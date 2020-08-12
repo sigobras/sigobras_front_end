a2d19e7b8810ed64587dcbc796e449a3330a05a3
ang95 <german.apaza.n@gmail.com>
2020-08-11 19:31:44 -0500

    filtros en metrados diaris ok

---
+++ src/js/components/Pfisicos/Metrados/Diarios/ComponentsDiarios/index.js
@@ -0,0 +1,7 @@
+import { MdMonetizationOn, MdVisibility, MdWatch, MdLibraryBooks } from 'react-icons/md';
+import { FaSuperpowers } from 'react-icons/fa';
+import { TiWarning } from "react-icons/ti";
+
+export {
+    MdMonetizationOn, MdVisibility, MdWatch, TiWarning, MdLibraryBooks, FaSuperpowers
+};
