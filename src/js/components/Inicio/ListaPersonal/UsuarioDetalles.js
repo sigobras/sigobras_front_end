3180933a07c5088244124d7d7b220e34052d921e
Luijhy Guerra <luijhy9234@gmail.com>
2021-02-22 12:04:21 -0500

    fix usuarios modal

---
+++ src/js/components/Inicio/ListaPersonal/UsuarioDetalles.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Row,
+  Col,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormFeedback,
+  ListGroup,
+} from "reactstrap";
+import axios from "axios";
+import { FaSearch } from "react-icons/fa";
+
+import { UrlServer } from "../../Utils/ServerUrlConfig";
+import Usuario from "../../../../images/usuario.png";
+export default ({ data }) => {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+  return (
+    <div>
+      <div onClick={() => toggle()} style={{ cursor: "pointer" }}>
+        {data.nombre}
+      </div>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader>{data.cargo_nombre}</ModalHeader>
+        <ModalBody>
+          <Row>
+            <Col md="6">
+              <img src={Usuario} className="w-75" alt="users sigobras" />
+            </Col>
+            <Col md="6">
+              <ListGroup>
+                <b>{data.nombre.toUpperCase()}</b>
+                <b>CELULAR N° </b> <span>{data.celular || "----"}</span>
+                <b>DNI </b> <span>{data.dni || "----"}</span>
+                <b>EMAIL </b> <span>{data.email || "----"}</span>
+                <b>DIRECCION </b> <span>{data.direccion || "----"}</span>
+                <b>N° COLEGIATURA </b> <span>{data.cpt || "----"}</span>
+              </ListGroup>
+            </Col>
+          </Row>
+        </ModalBody>
+      </Modal>
+    </div>
+  );
+};
