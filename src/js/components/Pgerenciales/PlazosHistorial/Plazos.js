19f1c8a75dce7d57b16d0bee0e8db95a11d0b19b
kaido <luijhy9234@gmail.com>
2021-04-26 12:50:37 -0500

    fix #544

--- src/js/components/Pgerenciales/PlazosHistorial/Plazos.js
+++ src/js/components/Pgerenciales/PlazosHistorial/Plazos.js
@@ -68,36 +68,7 @@ export default () => {
       fetchPLazosPadres();
     }
   }
-  //agregar archivo
-  const hiddenFileInput = useRef(null);
-  const [IdPlazo, setIdPlazo] = useState(0);
-  async function uploadFile(id) {
-    await setIdPlazo(id);
-    hiddenFileInput.current.click();
-  }
-  async function onChangeInputFile(e) {
-    if (confirm("desea subir la resolucion seleccionada?")) {
-      const formData = new FormData();
-      formData.append("archivo", e.target.files[0]);
-      e.target.value = null;
-      formData.append("codigo_obra", sessionStorage.getItem("codigoObra"));
-      formData.append("id_acceso", sessionStorage.getItem("idacceso"));
-      const config = {
-        headers: {
-          "content-type": "multipart/form-data",
-        },
-      };
-      var res = await axios.put(
-        `${UrlServer}/plazos/${IdPlazo}/archivo`,
-        formData,
-        config
-      );
-      console.log(res);
-      fetchPLazosPadres();
-    } else {
-      e.target.value = null;
-    }
-  }
+
   function DescargarArchivo(data) {
     if (confirm("Desea descargar el memorandum?")) {
       const url = data;
@@ -140,14 +111,6 @@ export default () => {
           </tr>
         </thead>
         <tbody>
-          <input
-            type="file"
-            ref={hiddenFileInput}
-            style={{
-              display: "none",
-            }}
-            onChange={(e) => onChangeInputFile(e)}
-          />
           {PlazosPadres.map((item, i) => [
             <tr
               key={i}
@@ -166,11 +129,12 @@ export default () => {
               </td>
               <td>
                 <TooltipUploadIcon
+                  key={JSON.stringify(item)}
                   item={item}
-                  uploadFile={uploadFile}
                   DescargarArchivo={DescargarArchivo}
                   UsuarioData={UsuarioData}
                   Permisos={Permisos}
+                  recargar={fetchPLazosPadres}
                 />
               </td>
               <td></td>
@@ -212,7 +176,6 @@ export default () => {
                 clone[item.id] = ref;
                 setRefPlazosHijos(clone);
               }}
-              uploadFile={uploadFile}
               DescargarArchivo={DescargarArchivo}
               UsuarioData={UsuarioData}
               Permisos={Permisos}
@@ -294,10 +257,7 @@ export default () => {
   );
 };
 const PLazosHijos = forwardRef(
-  (
-    { id_padre, count, uploadFile, DescargarArchivo, UsuarioData, Permisos },
-    ref
-  ) => {
+  ({ id_padre, count, DescargarArchivo, UsuarioData, Permisos }, ref) => {
     useImperativeHandle(ref, () => ({
       recarga() {
         fetchCargarPlazosHijos();
@@ -338,11 +298,12 @@ const PLazosHijos = forwardRef(
         <td>
           {
             <TooltipUploadIcon
+              key={JSON.stringify(item)}
               item={item}
-              uploadFile={uploadFile}
               DescargarArchivo={DescargarArchivo}
               UsuarioData={UsuarioData}
               Permisos={Permisos}
+              recargar={fetchCargarPlazosHijos}
             />
           }
         </td>
@@ -373,73 +334,109 @@ const PLazosHijos = forwardRef(
     ));
   }
 );
-function TooltipUploadIcon({ item, uploadFile, DescargarArchivo, Permisos }) {
+function TooltipUploadIcon({ item, DescargarArchivo, Permisos, recargar }) {
   const [tooltipOpen, setTooltipOpen] = useState(false);
   const [tooltipOpen2, setTooltipOpen2] = useState(false);
   const toggle = () => setTooltipOpen(!tooltipOpen);
   const toggle2 = () => setTooltipOpen2(!tooltipOpen2);
+  //agregar archivo
+  const hiddenFileInput = useRef(null);
+  const [IdPlazo, setIdPlazo] = useState(0);
+  async function uploadFile(id) {
+    await setIdPlazo(id);
+    hiddenFileInput.current.click();
+  }
+  async function onChangeInputFile(e) {
+    if (confirm("desea subir la resolucion seleccionada?")) {
+      const formData = new FormData();
+      formData.append("archivo", e.target.files[0]);
+      e.target.value = null;
+      formData.append("codigo_obra", sessionStorage.getItem("codigoObra"));
+      formData.append("id_acceso", sessionStorage.getItem("idacceso"));
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      };
+      var res = await axios.put(
+        `${UrlServer}/plazos/${IdPlazo}/archivo`,
+        formData,
+        config
+      );
+      recargar();
+    } else {
+      e.target.value = null;
+    }
+  }
   return (
     <div>
-      <Tooltip
-        placement="right"
-        isOpen={tooltipOpen}
-        target="TooltipExample"
-        toggle={toggle}
-      >
-        Adjuntar Resolución
-      </Tooltip>
-
-      <div style={{ display: "flex", textAlign: "center" }}>
-        {
-          <div
-            onClick={() => {
-              if (Permisos["actualizar_archivoplazo"]) {
-                uploadFile(item.id);
-              }
-            }}
-            style={{
-              cursor: "pointer",
-              width: "100%",
-            }}
-            id="TooltipExample"
-          >
-            <FaUpload size={15} color={"#ffffff"} />
-          </div>
-        }
-
-        {item.archivo !== null && [
-          <Tooltip
-            placement="right"
-            isOpen={tooltipOpen2}
-            target="TooltipExample2"
-            toggle={toggle2}
-          >
-            Descargar Resolución
-          </Tooltip>,
-          <div
-            className="text-primary"
-            onClick={() => DescargarArchivo(`${UrlServer}${item.archivo}`)}
-            style={{
-              cursor: "pointer",
-              width: "100%",
-            }}
-            id="TooltipExample2"
-          >
-            <FaFileContract size={20} color={"#17a2b8"} />
-          </div>,
-        ]}
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        style={{
+          display: "none",
+        }}
+        onChange={(e) => onChangeInputFile(e)}
+      />
+      <div style={{ textAlign: "center" }}>
+        {item.archivo ? (
+          <>
+            <Tooltip
+              placement="right"
+              isOpen={tooltipOpen2}
+              target="TooltipExample2"
+              toggle={toggle2}
+            >
+              Descargar Resolución
+            </Tooltip>
+            <div
+              className="text-primary"
+              onClick={() => DescargarArchivo(`${UrlServer}${item.archivo}`)}
+              style={{
+                cursor: "pointer",
+                width: "100%",
+              }}
+              id="TooltipExample2"
+            >
+              <FaFileContract size={20} color={"#17a2b8"} />
+            </div>
+            <div
+              style={{
+                fontWeight: "100",
+                color: "white",
+              }}
+            >
+              <div>Subido por : </div>
+              <div style={{ whiteSpace: "nowrap" }}>{item.usuario_nombre}</div>
+            </div>
+          </>
+        ) : (
+          <>
+            <Tooltip
+              placement="right"
+              isOpen={tooltipOpen}
+              target="TooltipExample"
+              toggle={toggle}
+            >
+              Adjuntar Resolución
+            </Tooltip>
+            <div
+              onClick={() => {
+                if (Permisos["actualizar_archivoplazo"]) {
+                  uploadFile(item.id);
+                }
+              }}
+              style={{
+                cursor: "pointer",
+                width: "100%",
+              }}
+              id="TooltipExample"
+            >
+              <FaUpload size={15} color={"#ffffff"} />
+            </div>
+          </>
+        )}
       </div>
-      {item.archivo !== null && (
-        <div
-          style={{
-            fontWeight: "100",
-            color: "white",
-          }}
-        >
-          <div>Subido por : </div>
-          <div style={{ whiteSpace: "nowrap" }}>{item.usuario_nombre}</div>
-        </div>
-      )}
     </div>
   );
 }
